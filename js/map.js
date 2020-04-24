7930b3e941a9650fda066b7c392db05426bc652b
harmonyforhope <63580503+harmonyforhope@users.noreply.github.com>
2020-04-23 22:31:42 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,3 +1,4 @@
+//CREATE MAP USING LEAFLET.JS
 var map = L.map('map').setView([20, -30], 3);
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
@@ -11,268 +12,69 @@ L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_toke
 	zoomOffset: -1
 }).addTo(map);
 
-/* CA */
-L.marker([36.987789, -119.7251616]).addTo(map)
-	.bindPopup("<b>California</b>" +	
-				"<br>Aaron Miller" +
-				"<br>Aaron Tu" +
-				"<br>Adrian Pu" +
-				"<br>Adrian Zaragoza" +
-				"<br>Andrew Yin" +
-				"<br>Andrew Zhao" +
-				"<br>Angela Tsang" +
-		   		"<br>April Lee" +
-				"<br>Ben Hoang" +
-				"<br>Catherine Huang" +
-				"<br>Charlotte Wong" +
-				"<br>Cindy Yan" +
-				"<br>Claire Liu" +
-				"<br>Coco Connor" +
-				"<br>Crystal Cheng" +
-				"<br>Daniel Mah" +
-				"<br>Elijah Wen" +
-				"<br>Eric Yan" +
-				"<br>Hannah Lin" +
-				"<br>Jeston Lu" +
-				"<br>Jocelyn Horng" +
-				"<br>Joey Vasconi" +
-				"<br>Joey Vasconi" +
-				"<br>Jonathan Wang Hou" +
-				"<br>Josephine Chan" +
-				"<br>Josie Chou" +
-				"<br>Justin Wahby " +
-				"<br>Kaoruko Hayashi " +
-				"<br>Karina Tseng" +
-				"<br>Kevin Yang" +
-				"<br>Lacey" +
-				"<br>Leslie Jin" +
-				"<br>Lukasz Yoder" +
-				"<br>Munan Cheng" +
-				"<br>Nancy Zheng" +
-				"<br>Nathaniel Zhang" +
-				"<br>Ophir Horovitz" +
-				"<br>Pablo Molano" +
-				"<br>Parker Van Ostrand" +
-				"<br>Qingping (Linda) Ye" +
-				"<br>Rebecca Tseng" +
-				"<br>Riley Kong" +
-				"<br>Roger Guo" +
-				"<br>Roger Xia" +
-				"<br>Ruiwen Ge" +
-				"<br>Ryan Jia" +
-				"<br>Ryan Sheng" +
-				"<br>Ryan Wang" +
-				"<br>Sarah Tuan" +
-				"<br>Simon Tao" +
-				"<br>Soda Hou" +
-				"<br>Solomon Ge" +
-				"<br>Soorya Kuppam" +
-				"<br>Spencer Cha" +
-				"<br>Vivian Wang" +			
-				"<br>Xiaodian" +
-				"<br>Xinran Shi" +
-				"<br>Yejin Song"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);
-				
-/* TX */
-L.marker([31.9686, -99.9018]).addTo(map)
-	.bindPopup("<b>Texas</b>" +
-				"<br>Ami Kitajima" +
-				"<br>Dylan Liu" +
-				"<br>Esther Pham"				
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
-				
-/* Kenya */
-L.marker([-0.023559, 37.9061928]).addTo(map)
-	.bindPopup("<b>Kenya</b>" +
-				"<br>Aaron Agola Jaoko"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
+//VARS
+var names = [];
+var locations = [];
+var loc = {};
 
-/* PA */
-L.marker([41.2033216, -77.1945247]).addTo(map)
-	.bindPopup("<b>Pennsylvania</b>" +
-				"<br>Addison Liu" +
-				"<br>Christian Chiu" +
-				"<br>Erica Clarke" +
-				"<br>Katie Liu" +
-				"<br>Nicholas Han"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
+//ADDS NAMES TO STATE/COUNTRY STRING (AUTO)
+function nameToLocation(name, location)
+{
+	var exist = false; var count = 0;
+	var keys = Object.keys(loc);
+	console.log(name, location, loc, keys);
+	for (var x=0; x<keys.length+1; x++)
+	{
+		if (location == keys[x]) { exist = true; }
+		if (exist == true) { loc[location] = loc[location] + name; exist=false; break; }
+	  else { count++; }
+		if (count == keys.length+1) { loc[location] = name; }
+	}
+}
 
-/* MD */
-L.marker([39.0457549, -76.6412712]).addTo(map)
-	.bindPopup("<b>Maryland</b>" +
-				"<br>Alena Lu" +
-				"<br>Elizabeth Qiu" +
-				"<br>Nadine Meister"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
+//ACCESS AIRTABLE API (GET DATA FROM AIRTABLE SPREADSHEET)
+var Airtable = require('airtable');
+var base = new Airtable({apiKey: 'keyQ7YUX3SUECVL4C'}).base('appSmUKDnFdEAT1YF');
 
-/* MO */
-L.marker([37.9642529, -91.8318334]).addTo(map)
-	.bindPopup("<b>Missouri</b>" +
-				"<br>Alex Chen" +
-				"<br>Jason Ziren Zhao"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
+base('Members').select({
+  view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+	//Fill arrays with data from spreadsheet
+  records.forEach(function(record) {
+		names.push(record.get('parsedName'));
+		locations.push(record.get('parsedLocation'));
+		nameToLocation(record.get('parsedName'), record.get('parsedLocation'));
+  });
 
-/* NY */
-L.marker([40.7127753, -74.0059728]).addTo(map)
-	.bindPopup("<b>New York</b>" +
-				"<br>Alex Nam" +
-				"<br>Ayane Nakajima" +
-				"<br>Benjamin T. Rossen" +
-				"<br>Caroline Hsu" +
-				"<br>Faith Chen" +
-				"<br>Julia Kim" +
-				"<br>Lucas Amory" +
-				"<br>Matthew Chen" +
-				"<br>Riley Barker"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
+  //ADD MAPS BY STATE/COUNTRY (ONLY MANUALLY FOR NOW)
+	L.marker([36.987789, -119.7251616]).addTo(map).bindPopup("<b>California</b>"+loc.CA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([31.9686, -99.9018]).addTo(map).bindPopup("<b>Texas</b>"+loc.TX, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([-0.023559, 37.9061928]).addTo(map).bindPopup("<b>Kenya</b>"+loc.Kenya, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([41.2033216,  -77.1945247]).addTo(map).bindPopup("<b>Pennsylvania</b>"+loc.PA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([39.0457549, -76.6412712]).addTo(map).bindPopup("<b>Maryland</b>"+loc.MD, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([37.9642529, -91.8318334]).addTo(map).bindPopup("<b>Missouri</b>"+loc.MO, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([40.7127753, -74.0059728]).addTo(map).bindPopup("<b>New York</b>"+loc.NY, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([40.0583238, -74.4056612]).addTo(map).bindPopup("<b>New Jersey</b>"+loc.NJ, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([32.1656221, -82.9000751]).addTo(map).bindPopup("<b>Georgia</b>"+loc.GA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([38.9071923, -77.0368707]).addTo(map).bindPopup("<b>Washington DC</b>"+loc["Washington D.C."], {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([37.4315734, -78.6568942]).addTo(map).bindPopup("<b>Virginia</b>"+loc.VA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([35.7595731, -79.0192997]).addTo(map).bindPopup("<b>North Carolina</b>"+loc.NC, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([40.6331249, -89.3985283]).addTo(map).bindPopup("<b>Illinois</b>"+loc.IL, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([27.6648274, -81.5157535]).addTo(map).bindPopup("<b>Florida</b>"+loc.FL, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([33.836081, -81.1637245]).addTo(map).bindPopup("<b>South Carolina</b>"+loc.SC, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([46.729553, -94.6858998]).addTo(map).bindPopup("<b>Minnesota</b>"+loc.MN, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([42.4072107, -71.3824374]).addTo(map).bindPopup("<b>Massachusetts</b>"+loc.MA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([47.7510741, -120.7401385]).addTo(map).bindPopup("<b>Washington</b>"+loc.WA, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([34.0489281, -111.0937311]).addTo(map).bindPopup("<b>Arizona</b>"+loc.AZ, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([40.4172871, -82.907123]).addTo(map).bindPopup("<b>Ohio</b>"+loc.OH, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([35.5174913, -86.5804473]).addTo(map).bindPopup("<b>Tennessee</b>"+loc.TN, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([51.253775, -85.323214]).addTo(map).bindPopup("<b>Ontario</b>"+loc.Ontario, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([36.2048225, 138.2529297]).addTo(map).bindPopup("<b>Japan</b>"+loc.Japan, {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([35.9077568, 127.766922]).addTo(map).bindPopup("<b>South Korea</b>"+loc["South Korea"], {'minWidth':'150', 'maxHeight':'200'});
+	L.marker([35.486703, 101.901875]).addTo(map).bindPopup("<b>China</b>"+loc.China, {'minWidth':'150', 'maxHeight':'200'});
 
-/* NJ */
-L.marker([40.0583238, -74.4056612]).addTo(map)
-	.bindPopup("<b>New Jersey</b>" +
-				"<br>Elizabeth Yang" +
-				"<br>Felicia He" +
-				"<br>Jenna Cooley" +
-				"<br>Steven Cui"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
+  fetchNextPage();
 
-/* GA */
-L.marker([32.1656221, -82.9000751]).addTo(map)
-	.bindPopup("<b>Georgia</b>" +
-				"<br>Alice Zhang" +
-				"<br>Charlie Li" +
-				"<br>Grace Peng" +
-				"<br>Skyler Feng" +
-				"<br>Yannie Tan"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-				
-/* WA DC */
-L.marker([38.9071923, -77.0368707]).addTo(map)
-	.bindPopup("<b>Washington DC</b>" +
-				"<br>Son Duong"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
-
-/* VA */
-L.marker([37.4315734, -78.6568942]).addTo(map)
-	.bindPopup("<b>Virginia</b>" +
-				"<br>Andrew Wu" +
-				"<br>Hana Wang"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
-							
-/* NC */
-L.marker([35.7595731, -79.0192997]).addTo(map)
-	.bindPopup("<b>North Carolina</b>" +
-				"<br>Andy Dai" +
-				"<br>Gabriel Crist" +
-				"<br>Will Aarons"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);
-				
-/* IL */
-L.marker([40.6331249, -89.3985283]).addTo(map)
-	.bindPopup("<b>Illinois</b>" +
-				"<br>Erin Yuan"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-
-/* FL */
-L.marker([27.6648274, -81.5157535]).addTo(map)
-	.bindPopup("<b>Florida</b>" +
-				"<br>Giancarlo Llerena"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);					
-				
-/* SC */
-L.marker([33.836081, -81.1637245]).addTo(map)
-	.bindPopup("<b>South Carolina</b>" +
-				"<br>Caleb Borick" +
-				"<br>Lauren Chen"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
-				
-/* MN */
-L.marker([46.729553, -94.6858998]).addTo(map)
-	.bindPopup("<b>Minnesota</b>" +
-				"<br>Aurora Wang"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-
-/* MA */
-L.marker([42.4072107, -71.3824374]).addTo(map)
-	.bindPopup("<b>Massachusetts</b>" +
-				"<br>Darryl Therrien" +
-				"<br>Rebekah Hong" +
-				"<br>Theo Teng"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-
-/* WA */
-L.marker([47.7510741, -120.7401385]).addTo(map)
-	.bindPopup("<b>Washington</b>" +
-				"<br>Deanna Han" +
-				"<br>Emily Jeeho Park" +
-				"<br>Jenna Tu" +
-				"<br>Quennie Nguyen"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);		
-
-/* AZ */
-L.marker([34.0489281, -111.0937311]).addTo(map)
-	.bindPopup("<b>Arizona</b>" +
-				"<br>Jessica Zhang" +
-				"<br>Jessica Zhang"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);
-
-/* OH */
-L.marker([40.4172871, -82.907123]).addTo(map)
-	.bindPopup("<b>Ohio</b>" +
-				"<br>Kasey Shao" +
-				"<br>Risa Chen" +
-				"<br>Sophia Suwiryo"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-
-/* TN */
-L.marker([35.5174913, -86.5804473]).addTo(map)
-	.bindPopup("<b>Tennessee</b>" +
-				"<br>Sophie Tan"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);	
-
-/* Ontario */
-L.marker([51.253775, -85.323214]).addTo(map)
-	.bindPopup("<b>Ontario</b>" +
-				"<br>Vanessa Yu"
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);					
-
-/* Japan */
-L.marker([36.2048225, 138.2529297]).addTo(map)
-	.bindPopup("<b>Japan</b>" +
-				"<br>Kaki Su"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);						
-
-/* South Korea */
-L.marker([35.9077568, 127.766922]).addTo(map)
-	.bindPopup("<b>South Korea</b>" +
-				"<br>Ben Kim"			
-				,{'minWidth':'150', 'maxHeight':'200'}
-				);				
-							
-$(window).on("resize", function() {
-    $("#map").height($(window).height()).width($(window).width());
-    map.invalidateSize();
-}).trigger("resize");				
+}, function done(err) {
+  	if (err) { console.error(err); return; }
+});
